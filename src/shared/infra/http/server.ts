e14c226df93566244a93a3bdfa6750370c6d5962
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-13 10:47:46 -0300

    add typeorm & config structure

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import '../typeorm';
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
