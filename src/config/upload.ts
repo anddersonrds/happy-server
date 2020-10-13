93b3c05038090cd98d46fab0713c8456c3a41479
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-13 16:50:42 -0300

    add AppError

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,11 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
-const uploadFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
+const uploadsPath = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
 export default {
+  uploadsFolder: uploadsPath,
   storage: multer.diskStorage({
-    destination: uploadFolder,
+    destination: uploadsPath,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,21 +1,36 @@
-import express from 'express';
-import path from 'path';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
-
-import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
-app.use(
-  '/uploads',
-  express.static(
-    path.join(__dirname, '..', '..', '..', '..', 'tmp', 'uploads'),
-  ),
+
+routes.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
 );
 
 app.listen(3333, () => {
